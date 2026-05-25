5dec64a6940a0b8a317e400c96e80bd3887cbbb0
joaogabrieldosantos <joaogabrieldosantos0901@gmail.com>
2026-05-25 13:18:52 -0300

    Create Ativ 08.cpp

---
+++ Ativ 08.cpp
@@ -0,0 +1,26 @@
+8) Faça um programa que peça várias notas (0 a 10) e pare de pedir quando o usuário informar um valor negativo.
+
+____________________________________________________________________________________________________________________
+
+#include < iostream>
+using namespace std;
+
+int main(){
+int notas;
+int i;
+
+
+do{
+	cout << "Me informe o valor de uma nota: ";
+	cin >> notas;
+	
+	if (notas > 10){
+		cout << "Valor invalido!! " << endl;
+		return 0;
+	}
+	
+	
+}while(notas >= 0);
+
+ return 0;
+}
