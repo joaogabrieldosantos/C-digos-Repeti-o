6e4e4d3e2d4dde5eda4d8405252a3aa35508a019
joaogabrieldosantos <joaogabrieldosantos0901@gmail.com>
2026-05-25 13:14:20 -0300

    Create Ativ 02.cpp

---
+++ Ativ 02.cpp
@@ -0,0 +1,54 @@
+2) Faça um programa que leia N números e informe o maior número.
+___________________________________________________________________________________________
+R:
+#include < iostream>
+using namespace std;
+
+int main(){
+	int n, nume, maior;
+	
+	cout << "Quantos numeros ira digitar? ";
+	cin >> n;
+	
+	cin >> maior;
+	
+	for(int i = 1; i < n; i++){
+		cin >> nume;
+		
+		if (nume > maior){
+			maior = nume;
+			
+		}
+	}
+	
+	cout << "O maior numero eh: " << maior << endl;
+	return 0;
+}
+
+OU
+
+#include < iostream>
+using namespace std;
+
+int main(){
+	int n = 0; 
+	int nume = 0; 
+	int maior = 0;
+	
+	cout << "Quantos numeros ira digitar? ";
+	cin >> n;
+	
+		
+	maior = nume;
+	for(int i = 1; i <= n; i++){
+		cin >> nume;
+		
+		if (nume > maior){
+			maior = nume;
+			
+		}
+	}
+	
+	cout << "O maior numero eh: " << maior << endl;
+	return 0;
+}
