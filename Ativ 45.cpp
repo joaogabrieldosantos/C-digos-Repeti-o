65eb9634bfa3f6ee5280807806b2382451b1db66
joaogabrieldosantos <joaogabrieldosantos0901@gmail.com>
2026-06-01 18:17:02 -0300

    Update Ativ 45.cpp
    Questao imensa

--- Ativ 45.cpp
+++ Ativ 45.cpp
@@ -27,199 +27,211 @@ ________________________________________________________________________________
 #include <iostream>
 using namespace std;
 
-int main(){
-	string name;
-	int nota;
-	char let;
-	char gab[10] = {'A', 'B', 'C', 'D', 'E', 'E', 'D', 'C', 'B', 'A'};
-	int continuar = 0;
-	float soma = 0;
-	float media = 0;
-	int maior = 0;
-	int menor = 9999999999999999999;
-	int notes[300];
-	
-	while(continuar == 0){
-		
+int main()
+{
+    string name;
+    int nota;
+    char let;
+    char gab[10] = {'A', 'B', 'C', 'D', 'E', 'E', 'D', 'C', 'B', 'A'};
 
-	
-	cout << "Seu nome eh: ";
-	getline(cin,name);
-	
-	cout << "Aviso as respostas so podem ser (A, B, C, D ou E)" << endl;
-	
-	
-	for(int i = 1; i <= 10; i++){
-		
-		cout << "Questao " << i << ":";
-		cin >> let;
-		
-		if (let != 'A' && let != 'a' && let != 'B' && let != 'b' && let != 'C' && let != 'c' && let != 'D' && let != 'd' && let != 'E' && let != 'e'){
-			cout << "Nao existe essa resposta, tente novamente!! " << endl;
-			
-		}else{
-		 switch(i){
-		 	case(1):
-		 	if(gab[0] = let){
-		 		
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 1 letra A" << endl;
-		 	nota = nota + 1;
-		 	
-		 }else{
-		 	cout << "Voce ERROU!" << endl;
-		 	cout << "A 1 letra A" << endl;
-		 	
-		 }
-		 
-		 break;
-		 
-		 case(2):
-		 	if (gab[1] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 2 letra B" << endl;
-		 		nota = nota + 1;
-		 		
-			 }else{
-			 	cout << "Voce ERROU!" << endl;
-			 	cout << "A 2 letra B" << endl;
-			 	
-			 }
-			 
-			 break;
-			 
-			 case(3):
-		 	if(gab[2] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 3 letra C" << endl;
-		 	nota = nota + 1;
-		 	
-		 }else{
-		 	cout << "Voce ERROU!" << endl;
-		 	cout << "A 3 letra C" << endl;
-		 	
-		 }
-		 
-		 break;
-		 
-		 case(4):
-		 	if (gab[3] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 4 letra D" << endl;
-		 		nota = nota + 1;
-		 		
-			 }else{
-			 	cout << "Voce ERROU!" << endl;
-			 	cout << "A 4 letra D" << endl;
-			 	
-			 }
-			 
-			 break;
-			 
-			 case(5):
-		 	if(gab[4] = let){
-		 		cout << "A 5 letra E" << endl;
-		 		cout << "Voce ACERTOU!!" << endl;
-		 	nota = nota + 1;
-		 	
-		 }else{
-		 	cout << "Voce ERROU!" << endl;
-		 	cout << "A 5 letra E" << endl;
-		 	
-		 }
-		 
-		 break;
-		 
-		 case(6):
-		 	if (gab[5] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 6 letra E" << endl;
-		 		nota = nota + 1;
-		 		
-			 }else{
-			 	cout << "Voce ERROU!" << endl;
-			 	cout << "A 6 letra E" << endl;
-			 	
-			 }
-			 
-			 break;
-			 
-			 case(7):
-		 	if(gab[6] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 7 letra D" << endl;
-		 	nota = nota + 1;
-		 	
-		 }else{
-		 	cout << "A 7 letra D" << endl;
-		 	cout << "Voce ERROU!" << endl;
-		 	
-		 }
-		 
-		 break;
-		 
-		 case(8):
-		 	if (gab[7] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 8 letra C" << endl;
-		 		nota = nota + 1;
-		 		
-			 }else{
-			 	cout << "A 8 letra C" << endl;
-			 	cout << "Voce ERROU!" << endl;
-			 	
-			 }
-			 
-			 break;
-			 
-			 case(9):
-		 	if(gab[8] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 9 letra B" << endl;
-		 	nota = nota + 1;
-		 	
-		 }else{
-		 	cout << "Voce ERROU!" << endl;
-			cout << "A 9 letra B" << endl;
-		 }
-		 
-		 break;
-		 
-		 case(10):
-		 	if (gab[9] = let){
-		 		cout << "Voce ACERTOU!!" << endl;
-		 		cout << "A 10 letra A" << endl;
-		 		nota = nota + 1;
-		 		
-			 }else{
-			 	cout << "Voce ERROU!" << endl;
-			 	cout << "A 10 letra A" << endl;
-			 	
-			 }
-			 
-			 break;
-		 
-		 default:
-		    cout << "Agr tem 11 questoes ne fdp?? " << endl;
-		    
-		 }
-		 
-		}
-		
-		
-		  
-	    
-	    
-	}
-	cout << "Deseja continuar?(0 para sim e 1 para nao) ";
-	cin >> continuar;
-	name = notes;
-}
-        for(int 0 = 1; o <= 10; o++){
-        	cout << "A nota da turma foi:" << endl;
-        	cout << "nota " << o << ": ";
-        	cout << notes;
-		}
+    int continuar = 0;
+    float soma = 0;
+    float media = 0;
 
+    int maior = 0;
+    int menor = 10;
 
+    int totalAlunos = 0;
+    int notes[300];
+
+    while(continuar == 0)
+    {
+        nota = 0;
+
+        cout << "Seu nome eh: ";
+        cin >> name;
+
+        cout << "Aviso: as respostas so podem ser (A, B, C, D ou E)" << endl;
+
+        for(int i = 1; i <= 10; i++)
+        {
+            cout << "Questao " << i << ": ";
+            cin >> let;
+
+            switch(i)
+            {
+                case 1:
+                    if(gab[0] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: A" << endl;
+                    break;
+
+                case 2:
+                    if(gab[1] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: B" << endl;
+                    break;
+
+                case 3:
+                    if(gab[2] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: C" << endl;
+                    break;
+
+                case 4:
+                    if(gab[3] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: D" << endl;
+                    break;
+
+                case 5:
+                    if(gab[4] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: E" << endl;
+                    break;
+
+                case 6:
+                    if(gab[5] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: E" << endl;
+                    break;
+
+                case 7:
+                    if(gab[6] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: D" << endl;
+                    break;
+
+                case 8:
+                    if(gab[7] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: C" << endl;
+                    break;
+
+                case 9:
+                    if(gab[8] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: B" << endl;
+                    break;
+
+                case 10:
+                    if(gab[9] == let)
+                    {
+                        cout << "Voce ACERTOU!!" << endl;
+                        nota++;
+                    }
+                    else
+                    {
+                        cout << "Voce ERROU!" << endl;
+                    }
+                    cout << "Resposta correta: A" << endl;
+                    break;
+            }
+        }
+
+        cout << "Aluno: " << name << endl;
+        cout << "Nota: " << nota << endl;
+
+        notes[totalAlunos] = nota;
+
+        soma = soma + nota;
+
+        if(nota > maior)
+        {
+            maior = nota;
+        }
+
+        if(nota < menor)
+        {
+            menor = nota;
+        }
+
+        totalAlunos++;
+
+        cout << "Deseja continuar?" << endl;
+        cout << "0 - Sim" << endl;
+        cout << "1 - Nao" << endl;
+        cin >> continuar;
+    }
+
+    media = soma / totalAlunos;
+
+    cout << "---------- RESULTADO FINAL ----------" << endl;
+    cout << "Maior acerto: " << maior << endl;
+    cout << "Menor acerto: " << menor << endl;
+    cout << "Total de alunos: " << totalAlunos << endl;
+    cout << "Media da turma: " << media << endl;
+
+    cout << "Notas dos alunos:" << endl;
+
+    for(int o = 0; o < totalAlunos; o++)
+    {
+        cout << "Aluno " << o + 1 << ": "
+             << notes[o] << " pontos" << endl;
+    }
+
+    return 0;
 }
